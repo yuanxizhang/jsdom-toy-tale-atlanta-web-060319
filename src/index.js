f559d1c92f1aaf4f26d14ca3edccbdac5cc07731
yuanxizhang <yuanxi.zhang@gmail.com>
2019-10-22 00:35:15 -0700

    Add GET toys feature.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const TOYS_URL = "http://localhost:3000/toys"
 let addToy = false
-
-// YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -14,5 +13,40 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetchToys();
+})
 
-// OR HERE!
+function fetchToys() {
+  return fetch(TOYS_URL)
+          .then(resp => resp.json())
+          .then (json => renderToys(json));  
+}
+
+function renderToys(json) {
+	json.forEach(toy => renderToyCard(toy));
+}
+
+function renderToyCard(toy) {
+	const all = document.getElementById("toy-collection");
+	const card = document.createElement("div");
+	card.setAttribute('class', "card");
+
+	const h = document.createElement("h2");
+	h.innerHTML = `${toy.name}`
+	const img = document.createElement("img");
+	img.className = "toy-avatar";
+	img.src = `${toy.image}`;
+
+	const p = document.createElement("p");
+	p.innerHTML = `${toy.likes} likes`
+	const likeBtn = document.createElement("button");
+	likeBtn.className = "like-btn";
+	likeBtn.innerHTML = "like";
+
+	all.appendChild(card);
+	card.appendChild(h);
+	card.appendChild(img);
+	card.appendChild(p);
+	card.appendChild(likeBtn);
+}
