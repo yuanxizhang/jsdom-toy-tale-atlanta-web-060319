bfc4f53fecb7311b692148c7784488f858922830
yuanxizhang <yuanxi.zhang@gmail.com>
2019-10-22 01:52:12 -0700

    update index.js with addToyCard function to handle POST request

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
   if (addToy) {
-    toyForm.style.display = 'block'
+    toyForm.style.display = 'block';
   } else {
     toyForm.style.display = 'none'
   }
 })
 
 document.addEventListener('DOMContentLoaded', () => {
+		let form = document.querySelector(".add-toy-form");
+  	form.addEventListener("submit", handleSubmit);
     fetchToys();
 })
 
@@ -23,20 +25,21 @@ function fetchToys() {
           .then (json => renderToys(json));  
 }
 
-function renderToys(json) {
-	json.forEach(toy => renderToyCard(toy));
+function renderToys(toys) {
+	toys.forEach((toy) => {renderToyCard(toy)});
 }
 
 function renderToyCard(toy) {
-	const all = document.getElementById("toy-collection");
+	const list = document.getElementById("toy-collection");
 	const card = document.createElement("div");
 	card.setAttribute('class', "card");
+	card.setAttribute("data-id", `${toy.id}`)
 
 	const h = document.createElement("h2");
-	h.innerHTML = `${toy.name}`
+	h.innerHTML = toy.name;
 	const img = document.createElement("img");
 	img.className = "toy-avatar";
-	img.src = `${toy.image}`;
+	img.src = toy.image;
 
 	const p = document.createElement("p");
 	p.innerHTML = `${toy.likes} likes`
@@ -44,9 +47,41 @@ function renderToyCard(toy) {
 	likeBtn.className = "like-btn";
 	likeBtn.innerHTML = "like";
 
-	all.appendChild(card);
+	list.appendChild(card);
+
 	card.appendChild(h);
 	card.appendChild(img);
 	card.appendChild(p);
 	card.appendChild(likeBtn);
 }
+
+function handleSubmit(e){
+  e.preventDefault()
+  let toyFormData = {
+  								name: e.target.name.value,
+     						  image: e.target.image.value
+  							}
+  addToyCard(toyFormData);
+
+}
+
+function addToyCard(toy) {
+	options = {
+		method: "POST",
+		headers: 
+						{
+							 "Content-Type": "application/json",
+								Accept: "application/json"
+						},
+		body: 	JSON.stringify({
+				      name: toy.name,
+				      image: toy.image,
+				      likes: 0
+				    })
+	}
+  fetch(TOYS_URL, options)
+          .then(resp => resp.json())
+          .then (json => renderToyCard(json));
+}
+
+
